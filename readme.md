3656adeeb563bc9a054bf207ceb3120bc424d6ac
François Pagé <francois.page@mail.mcgill.ca>
2024-11-14 23:12:36 -0500

    added a readme, should start having proper commit message from now on since i got the annoying thing setup, maybe merge all previous commit into one and have a proper message

---
+++ readme.md
@@ -0,0 +1,9 @@
+You will need to use the template script to copy the template files to their
+correct locations
+
+hyprpaper will be in ./
+
+but the other ones that are hacky, will be in ./machine-unique/
+
+You can modify the non template version, as it will allow you to
+use the same git branch for multiple machine. Saving unneeded manual merge of different branches
--- template/__copy_template.sh
+++ template/__copy_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp hyprpaper.conf.template ../hyprpaper.conf
-cp monitor.conf.template ../monitor.conf
-cp alt_sound.conf.template ../alt_sound.conf
-cp waybar_toggle.conf.template ../waybar_toggle.conf
+cp monitor.conf.template ../machine_unique/monitor.conf
+cp alt_sound.conf.template ../machine_unique/alt_sound.conf
+cp waybar_toggle.conf.template ../machine_unique/waybar_toggle.conf
