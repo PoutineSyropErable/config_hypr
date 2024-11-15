85526233f8ccb398c9637554d8943cf2b813589c
François Pagé <francois.page@mail.mcgill.ca>
2024-11-14 23:05:36 -0500

    template moved

--- copy_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp hyprpaper.conf.template hyprpaper.conf
-cp monitor.conf.template monitor.conf
-cp alt_sound.conf.template alt_sound.conf
-cp waybar_toggle.conf.template waybar_toggle.conf
---
+++ template/__copy_template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp hyprpaper.conf.template ../hyprpaper.conf
+cp monitor.conf.template ../monitor.conf
+cp alt_sound.conf.template ../alt_sound.conf
+cp waybar_toggle.conf.template ../waybar_toggle.conf
